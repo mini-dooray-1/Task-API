49c8de4a868451f7ef0acc3adf5a2e046034655d
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2024-01-27 18:22:22 +0900

    init spring project

---
+++ src/test/java/com/nhnacademy/team/dooray/taskapi/TaskapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.team.dooray.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
